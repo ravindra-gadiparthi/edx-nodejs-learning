e9e3cfffd48856d12cdbb2e1463af07eedd8f34d
Ravindra Gadiparthi <Ravindra.Gadiparthi@vcontractor.co.za>
2020-02-03 16:30:54 +0530

    reading from json files

--- module_2/mongo-data-migration/server.js
+++ module_2/mongo-data-migration/server.js
@@ -1,5 +1,7 @@
 const mongo= require('mongodb')
 const async= require('async')
+const customers=require('./m3-customer-data.json')
+const customerAddresses=require('./m3-customer-address-data.json')
 
 console.log('!!!! Migration Stared Ready!!!!')
 
@@ -7,46 +9,36 @@ const MongoClient =mongo.MongoClient
 
 const url='mongodb://localhost:27017'
 
+let tasks=[]
+
+const limit=Number(process.argv[2]) || 1000
+
 MongoClient.connect(url,(error,client)=>{
 	
-	//console.log("successfully connected to mongodb")
-	const db= client.db('test')
-	const customerData=db.collection('CustomerData')
-	const customerDataAddress=db.collection('CustomerAddresssData')
+	console.log("successfully connected to mongodb")
+	const customersCollection=client.db('test').collection("customers")
 	
-	const task= (addressData,start,end)=> {
-		console.log(`start ${start} and end ${end}`)
-		for(let i=start; i < end-1; i++)
-		{
-			
-			const address= addressData[i]
-			const data={"country":address.country,"city":address.city,"state":address.state,"phone":address.phone}
-			customerData.updateOne({id:`${i+1}`},{$set:data})
-		}
+	const saveCustomers =(data,callback)=>{
+			console.log(data.length)
+			customersCollection.insert(data,(error,results)=>{
+				callback(error,results)
+			})
 	}
 	
-	customerDataAddress.find({}).toArray((error,addressData)=>{
+	customers.forEach((customer,index,list)=>{
+		customers[index]=Object.assign(customer,customerAddresses[index])
 		
-			if(error) process.exit(-1)
-			const tasks=[]
-		    const recordsPerIteration=Number(process.argv[2])
-			//console.log(`requested items per iteration ${recordsPerIteration}`)
-			const totalRecords=addressData.length;
-			
-			const noOfIterations=totalRecords/recordsPerIteration
-			//console.log(`total records are ${totalRecords} and iterations ${noOfIterations}`)
-			for(let i=0;i<noOfIterations;i++)
-			{
-				const start=i*recordsPerIteration;
-				const end= start + recordsPerIteration; 
-				tasks.push(()=>task(addressData,start,end))
-			}
+		if(index % limit ===0)
+		{
+			const start=index;
+			const end = start+ limit < customers.length ? start + limit : customers.length;
+			console.log(`start ${start} and end ${end}`)
+			tasks.push((callback)=>saveCustomers(customers.slice(start,end),callback))
+		}
 		
-			async.parallel(tasks, (error, results) => {
+	})
+	console.log('launching tasks')
+	async.parallel(tasks, (error, results) => {
 					console.log('successfully executed tasks')
 			})
 	})
-	
-
-
-})
